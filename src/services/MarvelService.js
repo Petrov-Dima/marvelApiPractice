6559d2337295535817f15d321bc79f109bd551fc
Dima <dimapetrov78959@gmail.com>
2022-09-01 00:48:08 +0300

    useHooksInProject

---
+++ src/services/MarvelService.js
@@ -0,0 +1,35 @@
+import {useHttp} from '../hooks/http.hook.js'
+
+const useMarvelService = () => { 
+    const {loading, request, error, clearError} = useHttp();
+
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=d36bac3c56df5cd7c15cb039b6af926f';
+    const _baseOffset = 210;
+
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+
+    const _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...`: 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+
+    return {loading, error, clearError, getAllCharacters, getCharacter}
+}
+
+export default useMarvelService;
